97e4011ea41a56de63b879df3f56a20786695791
Sarah Stoffels <github@stoffels.it>
2021-10-13 00:32:23 +0200

    Initial testjob

---
+++ test-matrix-job-seed.groovy
@@ -0,0 +1,33 @@
+String folder1 = 'Test1/'
+String folder2 = 'Test2/'
+
+def proj_names = [
+    folder1 + 'Name1',
+    folder2 + 'Name2',
+]
+
+matrixJob('SarahTestjobMatrix') {
+    wrappers {
+        preBuildCleanup()
+    }
+    triggers {
+        upstream(proj_names.join(','), 'SUCCESS')
+    }
+    stages {
+        stage('Copy Archive') {
+            for (proj_name in proj_names) {
+                int i = proj_name.lastIndexOf('/');
+                String proj_target = proj_name.substring(i+1);
+                steps {
+                    script {
+                        step ([$class: 'CopyArtifact',
+                            projectName: '$proj_name',
+                            filter: "*.xml,**/.test",
+                            target: '$proj_target']);
+                    }
+                }
+            }
+        }
+    }
+}
+
